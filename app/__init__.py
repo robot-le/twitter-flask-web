bf7f4f3d39731bd4ea376df3d6f4edf7e434df1a
spacekiddo <example@mail.com>
2024-07-13 15:39:28 +0600

    Edit tests, add .env support, add requirements.txt file

--- app/__init__.py
+++ app/__init__.py
@@ -27,9 +27,9 @@ babel = Babel()
 detector = LanguageDetectorBuilder.from_all_languages().build()
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)
